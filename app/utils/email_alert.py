03259426fc686e4335a7f9200d9e6b4b6bbbf2a4
shubhankar <shubhankarsaket@gmail.com>
2025-12-17 09:16:23 +0530

    files added

--- app/utils/email_alert.py
+++ app/utils/email_alert.py
@@ -1,10 +1,10 @@
 from twilio.rest import Client
 import os
 
-SID = os.getenv("TWILIO_SID",  "AC4f7dd918bc6a747fc1df95efc0646c95")
-AUTH_TOKEN = os.getenv("TWILIO_TOKEN", "a04daa74191545b1f5072ed8fd2dce0f")
-FROM_NUMBER = os.getenv("TWILIO_FROM", "+16592225065") # e.g., +1234567890
-TO_NUMBER = os.getenv("USER_PHONE", "+916205582857")
+SID = os.getenv("TWILIO_SID")
+AUTH_TOKEN = os.getenv("TWILIO_TOKEN")
+FROM_NUMBER = os.getenv("TWILIO_FROM")
+TO_NUMBER = os.getenv("USER_PHONE")
 
 def send_twilio_alert(threat_type, timestamp):
     client = Client(SID, AUTH_TOKEN)
